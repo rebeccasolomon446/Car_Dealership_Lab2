e330648bb52437b05e8fb6494d8ca7d27a879ff2
Rebecca <rebecca.solomon446@gmail.com>
2023-01-14 14:46:49 +0000

    starts lab task

---
+++ car.js
@@ -0,0 +1,6 @@
+// Car constructor function
+const Car = function (manufacturer, price, engineType) {
+    this.manufacturer = manufacturer;
+    this.price = price;
+    this.engineType = engineType;
+}
