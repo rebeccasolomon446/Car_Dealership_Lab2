33aa8fdce7c59fa524847fbfbed5ae2c6d13b6c9
Rebecca <rebecca.solomon446@gmail.com>
2023-01-14 15:08:06 +0000

    completes test to access car1 object manufacturer

--- car.test.js
+++ car.test.js
@@ -5,5 +5,15 @@ const {Car} = require("./car");
 // before each function to set the foundations for testing
 beforeEach(() => {
     // initialise a Car object
-    Car = new Car("BMW 1 Series", 10000, "Hybrid")
+    car1 = new Car("BMW 1 Series", 10000, "Hybrid")
+})
+
+describe("manufacturer test", () => {
+    test("can access manufacturer info", () => {
+        expected = "BMW 1 Series";
+        actual = car1.manufacturer;
+        expect(actual).toBe(expected);
+    })
+    
+
 })
