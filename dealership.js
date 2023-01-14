2bab3fb19e3dd551696aaf748891cac4c344ddc0
Rebecca <rebecca.solomon446@gmail.com>
2023-01-14 22:51:00 +0000

    creates function in dealershi file to add cars to a dealership object

--- dealership.js
+++ dealership.js
@@ -1,12 +1,17 @@
 const Dealership = function(name, maxNumbOfCars, carsInStock){
     this.name = name;
     this.maxNumbOfCars = maxNumbOfCars;
-    this.carsInStock = carsInStock;
+    this.carsInStock = [];
 }
 
 // Method to count number of stock
 Dealership.prototype.countCars = function() {
     return this.carsInStock.length;
+}
+
+// Method to add cars to a dealership object
+Dealership.prototype.addCars = function(car) {
+    return this.carsInStock.push(car);
 }
 
 // export function for testing
--- dealership.test.js
+++ dealership.test.js
@@ -1,1 +1,30 @@
+const { Car } = require("./car");
 const {Dealership} = require("./dealership");
+
+let dealership1;
+
+beforeEach(() => {
+    // Initialise a dealership object
+   dealership1 = new Dealership("Reb's Dealership", 4);
+   // Initialise cars 
+   car1 = new Car("BMW 1 Series", 20000, "Hybrid")
+   car2 = new Car("Ford Focus", 150000, "Deisel");
+   car3 = new Car("Mercedes S Class", 46000, "Hybrid");
+})
+
+// describe("dealership tests", () => {
+
+//     // Testing if I can add cars to dealership1 object
+//     test("can add cars to a dealership object", () => {
+//         dealership1.addCar
+//         expected = 3;
+//         actual = 
+//     })
+
+//     // Testing countCars method
+//     test("can count the cars in stock at a dealership", () => {
+//         expected = 4;
+//         actual = dealership1.countCars();
+//         expect(actual).toBe(expected);
+//     })
+// })
