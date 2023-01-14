4a5e1cd9acf141d3fc005ecb61d02edc19415a35
Rebecca <rebecca.solomon446@gmail.com>
2023-01-14 22:05:15 +0000

    creates method using prototype to count the number of cars in stock

--- dealership.js
+++ dealership.js
@@ -3,3 +3,7 @@ const Dealership = function(name, maxNumbOfCars, carsInStock){
     this.maxNumbOfCars = maxNumbOfCars;
     this.carsInStock = carsInStock;
 }
+
+// Method to count number of stock
+Dealership.prototype.countCars = function() {
+    return this.carsInStock.length;
