2c534350e177962fdee84081483d18ae66aa5feb
Rebecca <rebecca.solomon446@gmail.com>
2023-01-14 22:55:11 +0000

    creates beforeeach function in dealership test file to initialise a dealership object, create cars, and add cars to it

--- dealership.test.js
+++ dealership.test.js
@@ -2,6 +2,9 @@ const { Car } = require("./car");
 const {Dealership} = require("./dealership");
 
 let dealership1;
+let car1;
+let car2;
+let car3;
 
 beforeEach(() => {
     // Initialise a dealership object
@@ -10,21 +13,24 @@ beforeEach(() => {
    car1 = new Car("BMW 1 Series", 20000, "Hybrid")
    car2 = new Car("Ford Focus", 150000, "Deisel");
    car3 = new Car("Mercedes S Class", 46000, "Hybrid");
+   // adds cars to dealership1
+   dealership1.addCar(car1);
+   dealership1.addCar(car2);
+   dealership1.addCar(car3);
 })
 
-// describe("dealership tests", () => {
+describe("dealership tests", () => {
 
-//     // Testing if I can add cars to dealership1 object
-//     test("can add cars to a dealership object", () => {
-//         dealership1.addCar
-//         expected = 3;
-//         actual = 
-//     })
+    // Testing if I can add cars to dealership1 object
+    // test("can add cars to a dealership object", () => {
+    //     expected = 3;
+    //     actual = 
+    // })
 
-//     // Testing countCars method
-//     test("can count the cars in stock at a dealership", () => {
-//         expected = 4;
-//         actual = dealership1.countCars();
-//         expect(actual).toBe(expected);
-//     })
-// })
+    // Testing countCars method
+    // test("can count the cars in stock at a dealership", () => {
+    //     expected = 4;
+    //     actual = dealership1.countCars();
+    //     expect(actual).toBe(expected);
+    // })
+})
