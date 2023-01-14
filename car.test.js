224d93001b1be6873b732dd5ebb357d41eacb1df
Rebecca <rebecca.solomon446@gmail.com>
2023-01-14 15:01:21 +0000

    beforeEach method to create a car object for testing purposes

--- car.test.js
+++ car.test.js
@@ -1,3 +1,9 @@
 // imports Car constructor function
 const {Car} = require("./car");
 
+
+// before each function to set the foundations for testing
+beforeEach(() => {
+    // initialise a Car object
+    Car = new Car("BMW 1 Series", 10000, "Hybrid")
+})
